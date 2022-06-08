1012cdfb041a81c100d62f1fbfd16f53efaaf4f0
prateek5916 <prateek.khandelwal@tothenew.com>
2022-06-08 17:01:41 +0530

    Update

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -5,7 +5,7 @@ const depends_on = "next_file"
 
 exports.upstreamTrigger = (downstream_pipeline_name, downstream_branch_name, dependsOn) => {
   return[{
-    label: ":hammer: Triggering ${downstream_pipeline_name} for branch ${downstream_branch_name}",
+    label: `:hammer: Triggering ${downstream_pipeline_name} for branch ${downstream_branch_name}`,
     trigger: downstream_pipeline_name,
     key: 'deploy',
     branches: downstream_branch_name,
