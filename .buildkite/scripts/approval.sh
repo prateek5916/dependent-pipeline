beec32b50c26b904878a38f74f2372c069b45a49
prateek5916 <prateek.khandelwal@tothenew.com>
2022-06-13 17:54:58 +0530

    updaate

--- .buildkite/scripts/approval.sh
+++ .buildkite/scripts/approval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-#APPROVAL_RESPONSE="$(buildkite-agent meta-data get "release_type")"
-#echo "approval: $APPROVAL_RESPONSE"
+APPROVAL_RESPONSE="$(buildkite-agent meta-data get "release_type")"
+echo "approval: $APPROVAL_RESPONSE"
 echo "My name is Khan"
