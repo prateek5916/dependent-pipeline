dd5402fb78649890c3c53cd8d26a83c42fa33c4e
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-21 14:28:25 +0530

    Slack Notify

--- .buildkite/slack.js
+++ .buildkite/slack.js
@@ -1,31 +1,32 @@
 #!/usr/bin/env node
 const { IncomingWebhook } = require('@slack/webhook');
+const channel = 'slack-notify';
+const url = 'https://hooks.slack.com/services/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'
+       
+//'https://hooks.slack.com/services/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'
 
- const channel = 'testing';
- const url = 'https://hooks.slack.com/services/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'
-       //'https://hooks.slack.com/services/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'
+function postMessage(text) {
+  const webhook = new IncomingWebhook(url);
+  try {
+    webhook.send({
+      channel,
+      text,
+      username: 'Buildkite',
+      icon_emoji: ':buildkite:',
+    });
+    console.log(`Message sent to ${channel}`);
+  } catch (e) {
+    console.log('e: ', e);
+  }
+};
 
-// function postMessage(text) {
-//   const webhook = new IncomingWebhook(url);
-//   try {
-//     webhook.send({
-//       channel,
-//       text,
-//       username: 'Buildkite',
-//       icon_emoji: ':buildkite:',
-//     });
-//     console.log(`Message sent to ${channel}`);
-//   } catch (e) {
-//     console.log('e: ', e);
-//   }
-// };
-const webhook = new IncomingWebhook(url);
+postMessage(":hammer: Hello!!!");
 
-// Send the notification
-(async () => {
-  await webhook.send({
-    text: 'I\'ve got news for you...',
-    channel,
-  });
-})();
-// postMessage(":hammer: Hello Ji");
+// // Send the notification
+// (async () => {
+//   await webhook.send({
+//     text: 'I\'ve got news for you...',
+//     channel,
+//   });
+// })();
+// // postMessage(":hammer: Hello Ji");
