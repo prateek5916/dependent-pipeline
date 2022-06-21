2acb089c2ee7741eaf6941ea1a8cb3527c5cc33e
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-21 14:56:23 +0530

    Updates

--- .buildkite/slack.js
+++ .buildkite/slack.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 const { IncomingWebhook } = require('@slack/webhook');
-const channel = 'slack-notify';
-const url = 'https://hooks.slack.com/services/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'
+const channel = 'slack-notify-1';
+const url = 'https://hooks.slack.com/services/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'
        
-//'https://hooks.slack.com/services/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'
-
 function postMessage(text) {
   const webhook = new IncomingWebhook(url);
   try {
@@ -20,13 +18,4 @@ function postMessage(text) {
   }
 };
 
-postMessage(":hammer: Hello!!!");
-
-// // Send the notification
-// (async () => {
-//   await webhook.send({
-//     text: 'I\'ve got news for you...',
-//     channel,
-//   });
-// })();
-// // postMessage(":hammer: Hello Ji");
+postMessage("Hello!!!")
