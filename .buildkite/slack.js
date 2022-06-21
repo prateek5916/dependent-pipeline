bd34d19836aab39b30f4a89d061a30af3e0ef7aa
prateek5916 <prateek.khandelwal@tothenew.com>
2022-06-21 11:26:02 +0530

    Slack Added

---
+++ .buildkite/slack.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const { IncomingWebhook } = require('@slack/webhook');
+
+const channel = 'buildkite-alert';
+const url = 'https://hooks.slack.com/services/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'
+
+postMessage = async (text) => {
+  const webhook = new IncomingWebhook(url);
+  try {
+    webhook.send({
+      channel,
+      text,
+      username: 'Buildkite',
+      icon_emoji: ':buildkite:',
+    });
+    console.log(`Message sent to ${channel}`);
+  } catch (e) {
+    console.log('e: ', e);
+  }
+};
+
+postMessage(":hammer: Hello Ji");
