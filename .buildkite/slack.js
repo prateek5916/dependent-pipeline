fc8e562c6b038eff02a7b6584dfebbe9c6e2db91
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-22 13:03:00 +0530

    Update slack.js

--- .buildkite/slack.js
+++ .buildkite/slack.js
@@ -1,21 +1,21 @@
-#!/usr/bin/env node
-const { IncomingWebhook } = require('@slack/webhook');
-const channel = 'slack-notify-1';
-const url = 'https://hooks.slack.com/services/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'
+// #!/usr/bin/env node
+// const { IncomingWebhook } = require('@slack/webhook');
+// const channel = 'slack-notify-1';
+// const url = 'https://hooks.slack.com/services/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'
        
-function postMessage(text) {
-  const webhook = new IncomingWebhook(url);
-  try {
-    webhook.send({
-      channel,
-      text,
-      username: 'Buildkite',
-      icon_emoji: ':buildkite:',
-    });
-    console.log(`Message sent to ${channel}`);
-  } catch (e) {
-    console.log('e: ', e);
-  }
-};
+// function postMessage(text) {
+//   const webhook = new IncomingWebhook(url);
+//   try {
+//     webhook.send({
+//       channel,
+//       text,
+//       username: 'Buildkite',
+//       icon_emoji: ':buildkite:',
+//     });
+//     console.log(`Message sent to ${channel}`);
+//   } catch (e) {
+//     console.log('e: ', e);
+//   }
+// };
 
-postMessage("Hello!!!")
+// postMessage("Hello!!!")
